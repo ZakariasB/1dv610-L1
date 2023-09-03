07b7d6736174ba5434e6764efc2e7c4127cce402
Zakarias Bergcrona <zb222cw@student.lnu.se>
2023-09-03 16:28:42 +0200

    Changes to web components

---
+++ src/js/components/greeting-app.js
@@ -0,0 +1,44 @@
+/**
+ * The greeting app web component module.
+ *
+ * @author Zakarias Bergcrona <zb222cw@student.lnu.se>
+ */
+
+const template = document.createElement('template')
+template.innerHTML = `
+<div>
+    Enter your name: <input type="text" id="usernameInput">
+    <button id="generateButton">Generate Greeting</button>
+</div>
+<pre id="greetingDisplay"></pre>
+` 
+customElements.define('greeting-app',
+/**
+ * Represents a nickname-form element.
+ */
+  class extends HTMLElement {
+    /**
+     * The html button element to generate a greeting.
+     */
+    generateButton
+    /**
+     * Creates an instance of the current type.
+     */
+    constructor () {
+      super()
+      this.attachShadow({ mode: 'open' }).appendChild(template.content.cloneNode(true))
+      this.generateButton = this.shadowRoot.getElementById('generateButton')
+      this.generateButton.addEventListener('click', () => {
+        const username = this.shadowRoot.getElementById('usernameInput').value;
+            const greeting = `
+          Hello, ${username}!
+          Here's a cat for you:
+
+ /\_/\  
+( o.o ) 
+> ^ <
+            `
+            this.shadowRoot.getElementById('greetingDisplay').textContent = greeting;
+      })
+    }
+  })
---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+/**
+ * The main script file of the application.
+ *
+ * @author Zakarias Bergcrona <zb222cw@student.lnu.se>
+ * @version 1.1.0
+ */
+import './components/greeting-app.js'
+const greetingApp = document.createElement('greeting-app')
+document.getElementById('app').appendChild(greetingApp)
+
+
+
+
+
+
